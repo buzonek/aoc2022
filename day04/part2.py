088f86840febe8868c406be5d41a5991f63e179a
buzonek <buzonekk@gmail.com>
2022-12-01 21:04:46 +0100

    Fixing errors

---
+++ day04/part2.py
@@ -0,0 +1,67 @@
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+
+    total = 0
+    for line in lines:
+        section_one, section_two = (
+            set(
+                range(
+                    int(
+                        item.split(
+                            '-',
+                        )[0],
+                    ), int(item.split('-')[1]) + 1,
+                ),
+            ) for item in line.split(',')
+        )
+        if section_one & section_two:
+            total += 1
+    return total
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
